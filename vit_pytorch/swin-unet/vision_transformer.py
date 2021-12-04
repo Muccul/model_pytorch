6b89666385faf973910d8e8a094cfea04db27963
v-jianliang <v-jianliang@microsoft.com>
2021-12-04 18:01:50 +0800

    jian

--- vit_pytorch/swin-unet/vision_transformer.py
+++ vit_pytorch/swin-unet/vision_transformer.py
@@ -27,22 +27,23 @@ class SwinUnet(nn.Module):
         self.zero_head = zero_head
         self.config = config
 
-        self.swin_unet = SwinTransformerSys(img_size=config.DATA.IMG_SIZE,
-                                patch_size=config.MODEL.SWIN.PATCH_SIZE,
-                                in_chans=config.MODEL.SWIN.IN_CHANS,
-                                num_classes=self.num_classes,
-                                embed_dim=config.MODEL.SWIN.EMBED_DIM,
-                                depths=config.MODEL.SWIN.DEPTHS,
-                                num_heads=config.MODEL.SWIN.NUM_HEADS,
-                                window_size=config.MODEL.SWIN.WINDOW_SIZE,
-                                mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
-                                qkv_bias=config.MODEL.SWIN.QKV_BIAS,
-                                qk_scale=config.MODEL.SWIN.QK_SCALE,
-                                drop_rate=config.MODEL.DROP_RATE,
-                                drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                                ape=config.MODEL.SWIN.APE,
-                                patch_norm=config.MODEL.SWIN.PATCH_NORM,
-                                use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+        self.swin_unet = SwinTransformerSys(img_size=224,  # 224
+                                            patch_size=4,  # 4
+                                            in_chans=3,
+                                            num_classes=self.num_classes,
+                                            embed_dim=96,
+                                            depths=[2, 2, 2, 2],
+                                            depths_decoder=[ 2, 2, 2, 1],
+                                            num_heads=[3, 6, 12, 24],
+                                            window_size=7,
+                                            mlp_ratio=4.,
+                                            qkv_bias=True,
+                                            qk_scale=None,
+                                            drop_rate=0.0,
+                                            drop_path_rate=0.1,
+                                            ape=False,
+                                            patch_norm=True,
+                                            use_checkpoint=False)
 
     def forward(self, x):
         if x.size()[1] == 1:
